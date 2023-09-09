db56583313a6c8cfebf286fbc65a6e1ae3838475
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2023-09-10 00:45:21 +0500

    Add files via upload

---
+++ ConsoleApplication1.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	int divisible, divider, incomplete_private, counter = 0;
+	printf("Enter divisible ");
+	scanf_s("%d", &divisible);
+	printf("Enter divider ");
+	scanf_s("%d", &divider);
+	divisible = abs(divisible);
+	divider = abs(divider);
+	if (divisible > divider)
+	{
+		while (divisible > divider)
+		{
+			divisible = divisible - divider;
+			counter += 1;
+		}
+		incomplete_private = counter;
+		printf("%d", incomplete_private);
+	}
+	else
+	{
+		printf("Error");
+	}
+}
+
